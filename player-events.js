d0537e7d224598e2e546d6664439ef353d01ad72
Shawn Biddle <shawncplus@gmail.com>
2018-10-26 23:07:34 -0400

    equip player event moved to bundle-example-commands with the equip command

--- player-events.js
+++ player-events.js
@@ -61,33 +61,5 @@ module.exports = {
 
       this.save();
     },
-
-    /**
-     * Handle a player equipping an item with a `stats` property
-     * @param {string} slot
-     * @param {Item} item
-     */
-    equip: state => function (slot, item) {
-      if (!item.metadata.stats) {
-        return;
-      }
-
-      const config = {
-        name: 'Equip: ' + slot,
-        type: 'equip.' + slot
-      };
-
-      const effectState = {
-        slot,
-        stats: item.metadata.stats,
-      };
-
-      this.addEffect(state.EffectFactory.create(
-        'equip',
-        this,
-        config,
-        effectState
-      ));
-    }
   }
 };

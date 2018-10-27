757ff2d6e7335ede8283d6360d0bcbd5ce82c29e
Shawn Biddle <shawncplus@gmail.com>
2018-10-27 03:46:03 -0400

    Fix bundle path

--- player-events.js
+++ player-events.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const sprintf = require('sprintf-js').sprintf;
-const LevelUtil = require('../ranvier-lib/lib/LevelUtil');
+const LevelUtil = require('../bundle-example-lib/lib/LevelUtil');
 const { Broadcast, Config, Logger } = require('ranvier');
 
 module.exports = {
